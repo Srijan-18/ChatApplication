5f83b4e41fb789ec0df347ce4c3647affd10df0e
Srijan-18 <srijan.mishrasir@example.com>
2020-10-10 13:31:20 +0530

    ADDED: Build for Chat Application.

---
+++ build/CMakeFiles/ChatApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatApp.dir/server/controller/main.cpp.o"
+  "CMakeFiles/ChatApp.dir/server/controller/server_controller.cpp.o"
+  "CMakeFiles/ChatApp.dir/server/view/server_view.cpp.o"
+  "ChatApp"
+  "ChatApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
