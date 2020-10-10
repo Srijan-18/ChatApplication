abdd705a117389e544f0ab06a3e70ee8081ec946
Srijan-18 <srijan.mishrasir@example.com>
2020-10-10 13:28:28 +0530

    ADDED: Controller files for server.

---
+++ server/controller/server_controller.cpp
@@ -0,0 +1,8 @@
+#include "server_controller.h"
+
+using namespace std;
+
+void ServerController::printWelcomeMessage()
+{
+    server_view.printWelcomeMessage();
+}
---
+++ server/controller/server_controller.h
@@ -0,0 +1,9 @@
+#include "../view/server_view.h"
+
+class ServerController
+{
+    ServerView server_view;
+
+public:
+    void printWelcomeMessage();
+};
