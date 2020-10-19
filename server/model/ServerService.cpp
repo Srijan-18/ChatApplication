68ad33e58d4231bbddd3920b808d9ec0788e05c7
PrajyotGajane <prajyotgajane@gmail.com>
2020-10-20 01:53:55 +0530

    [Prajyot] Added: create connection method in service class

---
+++ server/model/ServerService.cpp
@@ -0,0 +1,17 @@
+#include "ServerService.h"
+
+int ServerService::createConnection()
+{
+    ServerIp.sin_family = AF_INET;
+    ServerIp.sin_port = htons(54200);
+    ServerIp.sin_addr.s_addr = INADDR_ANY;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (bind(sock, (struct sockaddr *)&ServerIp, sizeof(ServerIp)) == -1)
+        printf("[-]Bind unsuccessful, error!! \n");
+    else
+        printf("[+]Server Started\n");
+
+    if (listen(sock, 2) == -1)
+        printf("[-]Listening failed \n");
+}
+
