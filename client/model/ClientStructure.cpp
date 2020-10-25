ed54d70f3a3e0403d3d48e9a532b451ba9641f34
PrajyotGajane <prajyotgajane@gmail.com>
2020-10-25 19:04:13 +0530

    [Prajyot] Added: Clients chatroom status variable

--- client/model/ClientStructure.cpp
+++ client/model/ClientStructure.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 struct Client
 {
-    int socket;
+    int socket = 0;
     string name;
     string password;
     bool online_status = false;
+    bool chatroom_status = false;
 };
