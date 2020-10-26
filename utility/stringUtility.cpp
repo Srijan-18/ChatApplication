8d5294061343e91bd11f7056400dca46cae7b975
Srijan-18 <srijan.mishrasir@gmail.com>
2020-10-26 10:17:14 +0530

    [Srijan] Added: Function to get pair name to store chats.

--- utility/stringUtility.cpp
+++ utility/stringUtility.cpp
@@ -1,16 +1,19 @@
 #pragma once
 #include <vector>
 #include <string>
+
+using namespace std;
+
 class StringUtility
 {
 public:
-    static std::vector<std::string> splitter(const std::string &client_response, std::string delimiter)
+    static vector<string> splitter(const string &client_response, string delimiter)
     {
         size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-        std::string token;
-        std::vector<std::string> res;
+        string token;
+        vector<string> res;
 
-        while ((pos_end = client_response.find(delimiter, pos_start)) != std::string::npos)
+        while ((pos_end = client_response.find(delimiter, pos_start)) != string::npos)
         {
             token = client_response.substr(pos_start, pos_end - pos_start);
             pos_start = pos_end + delim_len;
@@ -18,5 +21,12 @@ public:
         }
         res.push_back(client_response.substr(pos_start));
         return res;
+    }
+
+    string getPairName(string sender, string reciever)
+    {
+        if (sender.compare(reciever) > 0)
+            return sender + "--" + reciever;
+        return reciever + "--" + sender;
     }
 };
